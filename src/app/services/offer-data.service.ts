2c81209eea383944581b28ba2b08df2555a0c187
Sergey Malyuga <sergey@malyuga.ru>
2026-08-05 09:05:24 +0300

    feat(offer): добавить сервис для работы с офферами

---
+++ src/app/services/offer-data.service.ts
@@ -0,0 +1,25 @@
+import {inject, Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {Offer, OfferPreview} from '../core/models/offers';
+import {API_PATHS, APIRoute, BASE_URL} from '../core/constants/const';
+import {getDefaultHttpPipes} from '../core/utils/rxjs-operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class OfferDataService {
+  private http = inject(HttpClient);
+
+  public getOffers(): Observable<OfferPreview[]> {
+    return this.http.get<OfferPreview[]>(`${BASE_URL}/${APIRoute.OFFERS}`).pipe(...getDefaultHttpPipes<OfferPreview[]>());
+  }
+
+  public getOfferById(offerId: string): Observable<Offer> {
+    return this.http.get<Offer>(`${BASE_URL}/${APIRoute.OFFERS}/${offerId}`).pipe(...getDefaultHttpPipes<Offer>());
+  }
+
+  public getNearbyOffers(offerId: string): Observable<OfferPreview[]> {
+    return this.http.get<OfferPreview[]>(`${BASE_URL}/${APIRoute.OFFERS}/${offerId}/${API_PATHS.NEARBY}`).pipe(...getDefaultHttpPipes<OfferPreview[]>());
+  }
+}
