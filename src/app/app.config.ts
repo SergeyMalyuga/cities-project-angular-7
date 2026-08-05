30c5196da0ddcc931647e3cc2e46712550e14cc5
Sergey Malyuga <sergey@malyuga.ru>
2026-08-05 09:12:57 +0300

    build(config): подключить глобальное состояние

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,11 +4,14 @@ import {provideRouter} from '@angular/router';
 import {routes} from './app.routes';
 import {provideHttpClient, withInterceptorsFromDi} from '@angular/common/http';
 import {provideEffects} from '@ngrx/effects';
+import {provideStore} from '@ngrx/store';
+import {appReducer} from './store/app/app.reducer';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({eventCoalescing: true}),
     provideRouter(routes),
     provideHttpClient(withInterceptorsFromDi()),
-    provideEffects()
+    provideEffects(),
+    provideStore(appReducer),
   ]
 };
