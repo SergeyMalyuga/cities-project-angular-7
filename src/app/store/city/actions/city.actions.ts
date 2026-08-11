1dcd581f7b779e7e7ea74995f3e5d13a6ee4be4f
Sergey Malyuga <sergey@malyuga.ru>
2026-08-11 09:08:18 +0300

    feat(city): добавить состояние текущего города

---
+++ src/app/store/city/actions/city.actions.ts
@@ -0,0 +1,5 @@
+import {createAction, props} from '@ngrx/store';
+import {City} from '../../../core/models/city';
+
+export const changeCity = createAction('[City] Change City',
+  props<{ city: City }>());
---
+++ src/app/store/city/city.reducer.ts
@@ -0,0 +1,12 @@
+import {DEFAULT_CITY} from '../../core/constants/const';
+import {createReducer, on} from '@ngrx/store';
+import {changeCity} from './actions/city.actions';
+
+const initialState = DEFAULT_CITY;
+
+export const cityReducer = createReducer(
+  initialState,
+  on(changeCity, (state, {city}) => ({
+    ...state, ...city
+  }))
+)
