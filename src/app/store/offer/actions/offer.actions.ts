5b3364fb9707e5951d95abd35017287026682e4c
Sergey Malyuga <sergey@malyuga.ru>
2026-08-11 08:37:50 +0300

    feat(offer): создать actions загрузки офферов

---
+++ src/app/store/offer/actions/offer.actions.ts
@@ -0,0 +1,10 @@
+import {createAction, props} from '@ngrx/store';
+import {OfferPreview} from '../../../core/models/offers';
+import {HttpErrorResponse} from '@angular/common/http';
+
+export const loadOffers = createAction('[Offer] Load Offers');
+export const loadOffersSuccess = createAction('[Offer] Load Offer Success',
+  props<{offers: OfferPreview[]}>());
+export const loadOffersFailure = createAction('[Offer] Load Offer Failure',
+  props<{error: HttpErrorResponse}>())
+
