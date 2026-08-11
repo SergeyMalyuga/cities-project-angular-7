aa4e291ff9f4afde1594ccaf7fb2ff2ca592f71b
Sergey Malyuga <sergey@malyuga.ru>
2026-08-11 08:42:58 +0300

    feat(app): добавить загрузку офферов

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component, inject, OnInit} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+import {Store} from '@ngrx/store';
+import {loadOffers} from './store/offer/actions/offer.actions';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
 })
-export class AppComponent {
-  title = 'angular-template-project';
+export class AppComponent implements OnInit {
+  private store = inject(Store);
+
+  public ngOnInit(): void {
+    this.store.dispatch(loadOffers());
+  }
 }
