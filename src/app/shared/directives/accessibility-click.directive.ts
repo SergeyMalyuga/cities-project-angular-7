824307c27fb37fb7ff946a1d6d13e9500603dc56
Sergey Malyuga <sergey@malyuga.ru>
2026-08-13 16:20:37 +0300

    feat(accessibility-click): добавить директиву доступного клика

---
+++ src/app/shared/directives/accessibility-click.directive.ts
@@ -0,0 +1,24 @@
+import {Directive, EventEmitter, HostBinding, HostListener, Output} from '@angular/core';
+
+@Directive({
+  selector: '[appAccessibilityClick]'
+})
+export class AccessibilityClickDirective {
+  @Output() clicked = new EventEmitter();
+
+  @HostBinding('attr.tabindex')
+  tabIndex = 0;
+
+  @HostListener('click')
+  onClick() {
+    this.clicked.emit();
+  }
+
+  @HostListener('keydown', ['$event'])
+  onKeyDown(evt: KeyboardEvent): void {
+    if (evt.key === 'Enter' || evt.key === ' ') {
+      evt.preventDefault();
+      this.clicked.emit();
+    }
+  }
+}
