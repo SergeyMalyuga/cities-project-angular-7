fda2781d407f45d09ca3cf8ea3278a86e806b988
Sergey Malyuga <sergey@malyuga.ru>
2026-08-13 13:03:12 +0300

    feat(user): добавить состояние пользователя

---
+++ src/app/store/user/user.reducer.ts
@@ -0,0 +1,12 @@
+import {UserState} from '../../core/models/user.state';
+import {AuthorizationStatus, DEFAULT_USER} from '../../core/constants/const';
+import {createReducer} from '@ngrx/store';
+
+const initialState: UserState = {
+  authorizationStatus: AuthorizationStatus.UNKNOWN,
+  user: DEFAULT_USER,
+  isLoading: false,
+  error: null,
+}
+
+export const userReducer = createReducer(initialState);
