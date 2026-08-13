4c9fd6982e89cd51674ccdeebff37f7881178fb7
Sergey Malyuga <sergey@malyuga.ru>
2026-08-13 13:52:35 +0300

    feat(user): добавить effect проверки аутентификации пользователя

---
+++ src/app/store/user/effects/user.effects.ts
@@ -0,0 +1,26 @@
+import {inject, Injectable} from '@angular/core';
+import {Actions, createEffect, ofType} from '@ngrx/effects';
+import {UserService} from '../../../services/user.service';
+import * as UserActions from '../actions/user.actions';
+import {AuthService} from '../../../services/auth.service';
+import {catchError, map, of, switchMap} from 'rxjs';
+import {HttpErrorResponse} from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UserEffects {
+  private actions$ = inject(Actions);
+  private userService = inject(UserService);
+  private authService = inject(AuthService);
+
+  public checkAuth$ = createEffect(() =>
+    this.actions$.pipe(ofType(UserActions.checkAuth), switchMap(() => {
+      const token = this.authService.getToken();
+      if (token) {
+        this.userService.checkAuth().pipe(map(user => UserActions.checkAuthSuccess({user})),
+          catchError((error: HttpErrorResponse) => of(UserActions.checkAuthFailure(error))))
+      }
+      return of(UserActions.checkAuthFailure({error: 'Unauthorized'}));
+    })));
+}
