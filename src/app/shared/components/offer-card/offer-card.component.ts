84fda2bd967e5ff73600443cd5bdc48ac761d013
Sergey Malyuga <sergey@malyuga.ru>
2026-08-13 15:03:51 +0300

    feat(offer-card): добавить передачу события наведения/снятия курсора с карточки

--- src/app/shared/components/offer-card/offer-card.component.ts
+++ src/app/shared/components/offer-card/offer-card.component.ts
@@ -1,17 +1,28 @@
-import {ChangeDetectionStrategy, Component, Input} from '@angular/core';
+import {ChangeDetectionStrategy, Component, EventEmitter, Input, Output} from '@angular/core';
 import {OfferPreview} from '../../../core/models/offers';
 import {getRatingWidth} from '../../../core/utils/rating-width';
 import {TitleCasePipe} from '@angular/common';
+import {HoverTrackerDirective} from '../../directives/hover-tracker.directive';
 
 @Component({
   selector: 'app-offer-card',
   imports: [
-    TitleCasePipe
+    TitleCasePipe,
+    HoverTrackerDirective
   ],
   templateUrl: './offer-card.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class OfferCardComponent {
   @Input({required: true}) offer!: OfferPreview;
+  @Output() hovered = new EventEmitter<OfferPreview | null>();
+
   protected readonly getRatingWidth = getRatingWidth;
+
+  public onHovered(isHover: boolean): void {
+    if (isHover) {
+      return this.hovered.emit(this.offer);
+    }
+    return this.hovered.emit(null);
+  }
 }
