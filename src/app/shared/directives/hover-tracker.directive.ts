ffaf10c576165acd7dcb3dc6f0b47cf646a29b11
Sergey Malyuga <sergey@malyuga.ru>
2026-08-13 15:02:12 +0300

    feat(hover-tracker): добавить директиву для отслеживания наведения курсора на элемент

---
+++ src/app/shared/directives/hover-tracker.directive.ts
@@ -0,0 +1,18 @@
+import {Directive, EventEmitter, HostListener, Output} from '@angular/core';
+
+@Directive({
+  selector: '[appHoverTracker]',
+})
+export class HoverTrackerDirective {
+  @Output() hovered = new EventEmitter<boolean>();
+
+  @HostListener('mouseenter')
+  onMouseEnter(): void {
+    this.hovered.emit(true);
+  }
+
+  @HostListener('mouseleave')
+  onMouseLeave(): void {
+    this.hovered.emit(false);
+  }
+}
