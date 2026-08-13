ba2508fb8f3e0ca0a7edbbc427d993006ad242bd
Sergey Malyuga <sergey@malyuga.ru>
2026-08-13 14:45:35 +0300

    feat(home-page): отобразить список городов

--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -3,18 +3,23 @@ import {HeaderComponent} from '../../shared/components/header/header.component';
 import {Store} from '@ngrx/store';
 import {selectCurrentCity, selectOffersByCity} from '../../store/app/selectors/app.selectors';
 import {OfferCardComponent} from '../../shared/components/offer-card/offer-card.component';
+import {CITY_LOCATIONS} from '../../core/constants/const';
+import {NgClass} from '@angular/common';
 
 @Component({
   selector: 'app-main',
   imports: [
     HeaderComponent,
-    OfferCardComponent
+    OfferCardComponent,
+    NgClass
   ],
   templateUrl: './main.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class MainComponent {
   private store = inject(Store);
+
+  protected readonly CITY_LOCATIONS = CITY_LOCATIONS;
 
   public offers = this.store.selectSignal(selectOffersByCity);
   public currentCity = this.store.selectSignal(selectCurrentCity);
