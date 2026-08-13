0e07d0cebc6b713d8bac0bc307fd5ab76ff68920
Sergey Malyuga <sergey@malyuga.ru>
2026-08-13 13:01:06 +0300

    feat(user): создать сервис для работы с данными пользователя

---
+++ src/app/services/user.service.ts
@@ -0,0 +1,28 @@
+import {inject, Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {User} from '../core/models/user';
+import {APIRoute, BASE_URL} from '../core/constants/const';
+import {getDefaultHttpPipes} from '../core/utils/rxjs-operators';
+import {Credentials} from '../core/models/credentials';
+
+@Injectable({
+  providedIn: 'root',
+})
+
+export class UserService {
+  private http = inject(HttpClient);
+
+  public checkAuth(): Observable<User> {
+    return this.http.get<User>(`${BASE_URL}/${APIRoute.LOGIN}`).pipe(...getDefaultHttpPipes<User>());
+  }
+
+  public login(credentials: Credentials): Observable<User> {
+    return this.http.post<User>(`${BASE_URL}/${APIRoute.LOGIN}`, credentials).pipe(...getDefaultHttpPipes<User>());
+  }
+
+  public logout(): Observable<void> {
+    return this.http.delete<void>(`${BASE_URL}/${APIRoute.LOGOUT}`).pipe(...getDefaultHttpPipes<void>());
+  }
+}
+
