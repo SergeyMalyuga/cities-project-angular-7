c26684a5bba39933f4bc38b448f56b3d3b59a137
Sergey Malyuga <sergey@malyuga.ru>
2026-08-13 13:18:54 +0300

    feat(auth): создать сервис аутентификации

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,37 @@
+import {Injectable} from '@angular/core';
+import {Token} from '../core/models/token';
+import {AUTH_TOKEN_KEY_NAME} from '../core/constants/const';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  public getToken(): Token | null {
+    try {
+      return localStorage.getItem(AUTH_TOKEN_KEY_NAME);
+    } catch (error) {
+      console.error('Error get token from localstorage', error);
+      return null;
+    }
+  }
+
+  public setToken(token: Token): boolean {
+    try {
+      localStorage.setItem(AUTH_TOKEN_KEY_NAME, token);
+      return true;
+    } catch (error) {
+      console.error('Error set token from localstorage', error);
+      return false;
+    }
+  }
+
+  public removeToken(): boolean {
+    try {
+      localStorage.removeItem(AUTH_TOKEN_KEY_NAME);
+      return true;
+    } catch (error) {
+      console.error('Error remove token from localstorage', error);
+      return false;
+    }
+  }
+}
