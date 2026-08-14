6a44171594dec864a2906dc567dac15c62450eb8
Sergey Malyuga <sergey@malyuga.ru>
2026-08-14 11:55:31 +0300

    feat(login): настроить навигацию на главную страницу
    - Навигация происходит при клике на логотип

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,11 +1,16 @@
 import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {RouterLink} from '@angular/router';
+import {AppRoute} from '../../core/constants/const';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './login.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class LoginComponent {
 
+  protected readonly AppRoute = AppRoute;
 }
