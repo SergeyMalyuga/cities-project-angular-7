a63dc4b5fd8a4f20982450ed0fdbf3257b5690e9
Sergey Malyuga <sergey@malyuga.ru>
2026-08-14 11:56:50 +0300

    feat(header): настроить навигацию на страницу аутентификации
    - Навигация происходить при клике на кнопку Login

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -3,10 +3,14 @@ import {AppState} from '../../../core/models/app.state';
 import {Store} from '@ngrx/store';
 import {selectAuthStatus} from '../../../store/user/selectors/user.selector';
 import {isAuth} from '../../../core/utils/auth-status';
+import {AppRoute} from '../../../core/constants/const';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './header.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
@@ -15,4 +19,5 @@ export class HeaderComponent {
 
   public authStatus = this.store.selectSignal(selectAuthStatus);
   protected readonly isAuth = isAuth;
+  protected readonly AppRoute = AppRoute;
 }
