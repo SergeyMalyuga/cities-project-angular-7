263c821b86a2672fdd6b18a1ac1dbcbb7a03ef33
Sergey Malyuga <sergey@malyuga.ru>
2026-08-14 14:31:40 +0300

    feat(favorite-offer): добавить состояние избранных офферов

--- src/app/core/models/offers-favorite-state.ts
+++
@@ -1,7 +0,0 @@
-import { EntityState } from '@ngrx/entity';
-import { OfferPreview } from './offers';
-
-export interface FavoriteOffersState extends EntityState<OfferPreview> {
-  isLoading: boolean;
-  error: string | null;
-}
