fde99c60ad5b09e6bfb46d14faf01417d32bf82e
Sergey Malyuga <sergey@malyuga.ru>
2026-08-14 11:35:34 +0300

    feat(places-sorting-form): передавать выбранный тип сортировки родительскому компоненту

--- src/app/components/places-sorting-form/places-sorting-form.component.ts
+++ src/app/components/places-sorting-form/places-sorting-form.component.ts
@@ -1,19 +1,22 @@
-import {ChangeDetectionStrategy, Component, Input, signal} from '@angular/core';
+import {ChangeDetectionStrategy, Component, EventEmitter, Input, Output, signal} from '@angular/core';
 import {SortType} from '../../core/constants/const';
 import {NgClass} from '@angular/common';
 import {ToggleDirective} from '../../shared/directives/toggle.directive';
+import {AccessibilityClickDirective} from '../../shared/directives/accessibility-click.directive';
 
 @Component({
   selector: 'app-places-sorting-form',
   imports: [
     NgClass,
-    ToggleDirective
+    ToggleDirective,
+    AccessibilityClickDirective
   ],
   templateUrl: './places-sorting-form.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class PlacesSortingFormComponent {
   @Input({required: true}) currentSortType!: SortType;
+  @Output() clicked = new EventEmitter<SortType>();
 
   public sortTypes = Object.values(SortType);
   public isOptionsOpen = signal<boolean>(false);
@@ -23,6 +26,11 @@ export class PlacesSortingFormComponent {
   }
 
   public closeOptions() {
-      this.isOptionsOpen.set(false);
+    this.isOptionsOpen.set(false);
+  }
+
+  public onClicked(sortType: SortType) {
+    this.clicked.emit(sortType);
+    this.closeOptions();
   }
 }
