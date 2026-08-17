c9961623b302fae65dcb80e5d9aba11f5d6f2d84
Sergey Malyuga <sergey@malyuga.ru>
2026-08-17 12:55:11 +0300

    feat(favorite-offer): добавить actions изменение статуса избранности оффера

--- src/app/store/favorite-offer/actions/favorite-offer.actions.ts
+++ src/app/store/favorite-offer/actions/favorite-offer.actions.ts
@@ -1,9 +1,16 @@
 import {createAction, props} from '@ngrx/store';
-import {OfferPreview} from '../../../core/models/offers';
+import {Offer, OfferPreview} from '../../../core/models/offers';
 import {HttpErrorResponse} from '@angular/common/http';
 
 export const loadFavoriteOffers = createAction('[Favorite Offer] Load Favorite Offers');
 export const loadFavoriteOffersSuccess = createAction('[Favorite Offer] Load Favorite Offers Success',
   props<{ favoriteOffers: OfferPreview[] }>());
 export const loadFavoriteOffersFailure = createAction('[Favorite Offer] Load Favorite Offers Failure',
+  props<{ error: HttpErrorResponse }>());
+
+export const toggleFavoriteOffer = createAction('[Favorite Offer] Toggle Offer',
+  props<{ offerId: string, isFavorite: boolean }>());
+export const toggleFavoriteOfferSuccess = createAction('[Favorite Offer] Toggle Offer Success',
+  props<{ offer: Offer }>());
+export const toggleFavoriteOfferFailure = createAction('[Favorite Offer] Toggle Offer Failure',
   props<{ error: HttpErrorResponse }>());
