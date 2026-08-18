bd4d97be1e1381757f5fb43f874d1cd0237e8dd6
Sergey Malyuga <sergey@malyuga.ru>
2026-08-18 11:53:47 +0300

    feat(offer): добавить изменение статуса избранности оффера

--- src/app/pages/offer/offer.component.ts
+++ src/app/pages/offer/offer.component.ts
@@ -14,19 +14,19 @@ import {
   catchError,
   combineLatest,
   EMPTY,
-  filter,
+  filter, first,
   map,
   merge,
   of,
   Subject,
-  switchMap,
+  switchMap, tap,
 } from 'rxjs';
 import {OfferDataService} from '../../core/services/offer-data.service';
 import {AppRoute, QUANTITY_FIRST_OFFERS} from '../../core/constants/const';
 import {takeUntilDestroyed} from '@angular/core/rxjs-interop';
 import {LoaderComponent} from '../../shared/components/loader/loader.component';
 import {getRatingWidth} from '../../core/utils/rating-width';
-import {DatePipe, SlicePipe, TitleCasePipe} from '@angular/common';
+import {DatePipe, NgClass, SlicePipe, TitleCasePipe} from '@angular/common';
 import {Comment} from '../../core/models/comments';
 import {CommentService} from '../../core/services/comment.service';
 import {CommentFormComponent} from '../../components/comment-form/comment-form.component';
@@ -37,6 +37,7 @@ import {Store} from '@ngrx/store';
 import {AppState} from '../../core/models/app.state';
 import {selectAuthStatus} from '../../store/user/selectors/user.selector';
 import {isAuth} from '../../core/utils/auth-status';
+import {OfferService} from '../../core/services/offer.service';
 
 @Component({
   selector: 'app-offer',
@@ -50,6 +51,7 @@ import {isAuth} from '../../core/utils/auth-status';
     MapComponent,
     SlicePipe,
     OfferCardComponent,
+    NgClass,
   ],
   templateUrl: './offer.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -58,12 +60,14 @@ export class OfferComponent implements OnInit {
   private activatedRoute = inject(ActivatedRoute);
   private router = inject(Router);
   private offerDataService = inject(OfferDataService);
+  private offerService = inject(OfferService);
   private commentService = inject(CommentService);
   private destroyRef = inject(DestroyRef);
   private store = inject(Store<AppState>);
 
   protected readonly getRatingWidth = getRatingWidth;
   protected readonly QUANTITY_FIRST_OFFERS = QUANTITY_FIRST_OFFERS;
+  protected readonly isAuth = isAuth;
 
   public offer = signal<Offer | null>(null);
   private refreshOffer$ = new Subject<void>();
@@ -76,6 +80,7 @@ export class OfferComponent implements OnInit {
 
   public offerId = computed<string | null>(() => this.offer()?.id ?? null);
   public authStatus = this.store.selectSignal(selectAuthStatus);
+  public isLoading = signal<boolean>(false);
 
   public ngOnInit(): void {
     this.activatedRoute.paramMap
@@ -126,12 +131,34 @@ export class OfferComponent implements OnInit {
         this.offer.set(result.offer);
         this.comments.set(result.comments);
         this.nearbyOffers.set(result.nearbyOffers);
+        this.isLoading.set(false);
       });
+  }
+
+  public toggleFavoriteOffer() {
+    const offer = this.offer();
+    if (offer) {
+      this.isLoading.set(true);
+      this.offerService.toggleFavorite(offer.id, offer.isFavorite)
+        .pipe(first(status => status !== null),
+          tap((status) => {
+            if (status) {
+              this.refreshOffer$.next();
+            } else {
+              this.isLoading.set(false);
+            }
+          }),
+          catchError(() => {
+            this.isLoading.set(false);
+            return EMPTY;
+          }),
+          takeUntilDestroyed(this.destroyRef),
+        )
+        .subscribe();
+    }
   }
 
   public refreshComments(): void {
     this.refreshComment$.next();
   }
-
-  protected readonly isAuth = isAuth;
 }
